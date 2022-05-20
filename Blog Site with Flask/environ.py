6e9959cf0938a7f30e8757f5124e6813de7402a6
BALAJI UDAYAKUMAR <92335003+balajiuday2000@users.noreply.github.com>
2022-05-20 13:45:39 -0400

    Add files via upload

---
+++ Blog Site with Flask/environ.py
@@ -0,0 +1,7 @@
+import os
+
+a = os.environ.get('EMAIL_USER')
+b = os.environ.get('EMAIL_PASS')
+
+print(a)
+print(b)
---
+++ Blog Site with Flask/run.py
@@ -0,0 +1,4 @@
+from hope import app
+
+if __name__ == '__main__':
+    app.run(debug = True)
